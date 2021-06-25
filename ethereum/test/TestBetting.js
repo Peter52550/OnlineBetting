4b41aa84eb376d4d9004eb7a40dbf8088b32fd7a
howardchou0302 <howardchou0302@gmail.com>
2021-06-25 21:54:59 +0800

    modify hot

--- ethereum/test/TestBetting.js
+++ ethereum/test/TestBetting.js
@@ -10,6 +10,7 @@ contract('Peter', (accounts) => {
 
   it('TestAddBet', async () => {
     await token.setVIP({from: accounts[0], value: web3.utils.toWei("0.5")});
+    await token.setVIP({from: accounts[1], value: web3.utils.toWei("0.5")});
     await token.addBet(
       "asdf",
       0,
@@ -34,11 +35,35 @@ contract('Peter', (accounts) => {
       "Sports", 
       {from : accounts[0]}
     );
+    await token.addBet(
+      "Peter",
+      0,
+      9000,
+      99999999999999,
+      99999999999999,
+      99999999999999,
+      ["asd", "sdf"],
+      "China", 
+      "Meme", 
+      {from : accounts[1]}
+    );
+
+    await token.addMoney(1, 1, 100, {from: accounts[2], value: web3.utils.toWei("0.1")});
+    await token.addMoney(1, 1, 100, {from: accounts[0], value: web3.utils.toWei("0.1")});
+    await token.addMoney(2, 1, 200, {from: accounts[2], value: web3.utils.toWei("0.2")});
+    
     const ids= await token.getIds.call({from: accounts[0]});
     const bets = await token.getBets.call({from: accounts[0]});
     const hotBets = await token.getHotBets.call({from: accounts[0]});
-    console.log(ids[1][0].toNumber());
-    console.log(bets);
+    const member0 = await token.getMemberView.call({from: accounts[0]});
+    const member1 = await token.getMemberView.call({from: accounts[1]});
+    const member2 = await token.getMemberView.call({from: accounts[2]});
+
+    //console.log(ids[1][0].toNumber());
+    //console.log(bets);
     console.log(hotBets);
+    console.log(member0);
+    console.log(member1);
+    console.log(member2);
   })
 })
